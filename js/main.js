34ec1eb77014df0c719872c9ceac141abf0962ce
Austin Weale <austinweale@gmail.com>
2015-10-27 10:00:18 -0700

    less redundant

--- js/main.js
+++ js/main.js
@@ -4,82 +4,63 @@
 
 window.onload = function(){
 
-    $('#change-content').click(changeContent);
+    $('#change-content').click(function(){ selectVal(changeContent) });
 
-    $('#add-at-end').click(addContent);
+    $('#add-at-end').click(function(){ selectVal(addContent) });
 
-    $('#add-at-start').click(addFront);
+    $('#add-at-start').click(function(){ selectVal(addFront) });
 
-    $('#insert-before').click(insertBefore);
+    $('#insert-before').click(function(){ selectVal(insertBefore) });
 
-    $('#move-after').click(moveToAfter);
+    $('#move-after').click(function(){ selectVal(moveToAfter) });
 
-    $('#surround-class').click(surround);
+    $('#surround-class').click(function(){ selectVal(surround) });
 
-    $('#hide-text').click(longText);
+    $('#hide-text').click(function(){ selectVal(longText) });
 
-    $('#remove-word').click(remove);
+    $('#remove-word').click(function(){ selectVal(remove) });
 
 };
 
-
-var changeContent = function(){
+var selectVal = function(next){
     var selector = $('#selector').val();
     var newContent= $('#newContent').val();
+    next(selector, newContent);
+}
+
+var changeContent = function(selector, newContent){
     $(selector).html(newContent);
 }
 
-var addContent = function(){
-    var selector = $('#selector').val();
-    var newContent= $('#newContent').val();
+var addContent = function(selector, newContent){
     $(selector).append(newContent);
 }
 
-var addFront = function(){
-    var selector = $('#selector').val();
-    var newContent= $('#newContent').val();
+var addFront = function(selector, newContent){
     $(selector).prepend(newContent);
 }
 
-var insertBefore = function(){
-    var selector = $('#selector').val();
-    var newContent= $('#newContent').val();
-
+var insertBefore = function(selector, newContent){
     $(selector).before(newContent);
 }
 
-var moveToAfter = function(){
-    var selector = $('#selector').val();
-    var newContent= $('#newContent').val();
-
+var moveToAfter = function(selector, newContent){
     $(selector).insertAfter(newContent);
 }
 
-var surround = function(){
-    var selector = $('#selector').val();
-    var newContent= $('#newContent').val();
-
+var surround = function(selector, newContent){
     $(selector).wrap('<div class =' + newContent + '></div>');
 }
 
-var longText = function(){
-    var selector = $('#selector').val();
-    
+var longText = function(selector, newContent){
     var html = $(selector).html();
-
-
     if(html.length > 12){
         $(selector).wrap("<div class ='hidden'></div>");
     }
 }
 
-var remove = function(){
-    var selector = $('#selector').val();
-    var newContent= $('#newContent').val();
-
+var remove = function(selector, newContent){
     var html = $(selector).html();
-
-
     if(html.includes(newContent)){
         $(selector).remove();
     }
